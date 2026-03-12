7f2d71841a466e33a6915fc860909020ca72f72e
ayabidi <145360811+ayabidi@users.noreply.github.com>
2026-03-12 18:38:59 +0100

    Update server.py

--- server.py
+++ server.py
@@ -58,4 +58,5 @@ def analyze():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render définit ce PORT automatiquement
+    app.run(host="0.0.0.0", port=port)
